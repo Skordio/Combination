0ee50b13ed9b046d8dd1be410c82538b16c44bcc
Skordio <skordion@hotmail.com>
2023-02-09 19:17:44 -0500

    feat: dice roller works now at a basic level
    it doesn't have any animation or anything, but it displays 3 random
    numbers on dice

--- Combination 0.3/Assets/Scripts/DieRoller.cs
+++ Combination 0.3/Assets/Scripts/DieRoller.cs
@@ -5,10 +5,27 @@ using UnityEngine.UI;
 
 public class DieRoller : MonoBehaviour
 {
+    private bool rolling;
+
+    private Image[] diceFaceImageComponentReferences;
+    
+    public Sprite[] DiceFaces;
+
+    //0 will be green, 1 will be red and 2 will be blue
+    public int[] DiceValues;
+
+    public static int DICE_COUNT = 3;
+
     // Start is called before the first frame update
     void Start()
     {
-        DiceValues = new int[3];
+        DiceValues = new int[DICE_COUNT];
+        diceFaceImageComponentReferences = new Image[DICE_COUNT];
+        rolling = false;
+        for(int i = 0; i < diceFaceImageComponentReferences.Length; i++)
+        {
+            diceFaceImageComponentReferences[i] = transform.GetChild(i).GetComponent<Image>();
+        }
     }
 
     // Update is called once per frame
@@ -17,15 +34,10 @@ public class DieRoller : MonoBehaviour
         
     }
 
-    //0 will be green, 1 will be red and 2 will be blue
-    public int[] DiceValues;
-
     public int getDieValue(int diskNumber)
     {
         return DiceValues[diskNumber - 1];
     }
-
-    public Sprite[] DiceFaces;
 
     public void RollTheDice() 
     {
@@ -35,6 +47,8 @@ public class DieRoller : MonoBehaviour
 
         //For now going to use random number generation and show images
 
+        rolling = true;
+
         for (int i = 0; i < DiceValues.Length; i++)
         {
             DiceValues[i] = Random.Range( 1, 7 );
@@ -42,37 +56,37 @@ public class DieRoller : MonoBehaviour
             //Update Visuals in here to set dice roll in real time
             //TODO: add dice rolling animation of switching between multiple faces
 
-            Debug.Log(this.transform);
+            Debug.Log("Child count: " + transform.childCount);
+            Debug.Log("Child 1: " + transform.GetChild(0));
+            Debug.Log("Child 2: " + transform.GetChild(1));
+            Debug.Log("Child 3: " + transform.GetChild(2));
+            Debug.Log("DiceValues length: " + DiceValues.Length);
+            Debug.Log("GetChild number " + (DiceValues.Length-1 - i));
+            Debug.Log("GetChild with number: " + transform.GetChild(DiceValues.Length-1 - i));
 
             if (DiceValues[i] == 1)
             {
-                this.transform.GetChild(DiceValues.Length-1 - i).GetComponent<Image>().sprite =
-                    DiceFaces[0];
+                diceFaceImageComponentReferences[i].sprite = DiceFaces[0];
             }
             else if (DiceValues[i] == 2)
             {
-                this.transform.GetChild(DiceValues.Length-1 - i).GetComponent<Image>().sprite =
-                    DiceFaces[1];
+                diceFaceImageComponentReferences[i].sprite = DiceFaces[1];
             }
             else if (DiceValues[i] == 3)
             {
-                this.transform.GetChild(DiceValues.Length-1 - i).GetComponent<Image>().sprite =
-                    DiceFaces[2];
+                    diceFaceImageComponentReferences[i].sprite = DiceFaces[2];
             }
             else if (DiceValues[i] == 4)
             {
-                this.transform.GetChild(DiceValues.Length-1 - i).GetComponent<Image>().sprite =
-                    DiceFaces[3];
+                diceFaceImageComponentReferences[i].sprite = DiceFaces[3];
             }
             else if (DiceValues[i] == 5)
             {
-                this.transform.GetChild(DiceValues.Length-1 - i).GetComponent<Image>().sprite =
-                    DiceFaces[4];
+                diceFaceImageComponentReferences[i].sprite = DiceFaces[4];
             }
             else if (DiceValues[i] == 6)
             {
-                this.transform.GetChild(DiceValues.Length-1 - i).GetComponent<Image>().sprite =
-                    DiceFaces[5];
+                diceFaceImageComponentReferences[i].sprite = DiceFaces[5];
             }
         }
 
